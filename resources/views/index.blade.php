12f2a1a7a8bdcc5ffe9902eb7572634c6a473db3
laminegaci <mohamed61lamine@gmail.com>
2021-07-27 15:22:15 +0100

    push for delete folder

---
+++ resources/views/index.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <div class="col-md-6"></div>
+                <div class="col-md-6"></div>
+    <div class="col-md-6"></div>
+
+</div>
+</body>
+</html>
